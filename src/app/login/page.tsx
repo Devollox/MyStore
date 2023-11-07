1315c41359f90fae180fb1f2f649c1cdeb108fe5
Devollox.fun <122895078+Devollox@users.noreply.github.com>
2023-11-07 18:31:10 +0300

    Add files via upload

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import {Metadata} from "next";
 import NotFound from "@/app/error/page";
 
+import './login.sass'
+
 export const metadata: Metadata = {
     title: 'Login Console',
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,8 @@
 import {Metadata} from "next";
 import NotFound from "@/app/error/page";
+
+import './signup.sass'
+
 export const metadata: Metadata = {
     title: 'Signup Console',
 }
